02825b5908ae36be863d1abd0a5fc1bd5d0957bd
Anas Yaseen <anasyaseen1@outlook.com>
2022-05-01 10:04:27 +0300

    Moved translations to separate folder

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-const translations = {
-  en: {
-    home: "Home",
-    about: "About",
-    contact: "Contact us",
-    selectLanguage: "Select a language",
-    english: "English",
-    arabic: "Arabic",
-  },
-  ar: {
-    home: "الصفحة الرئيسية",
-    about: "من نحن",
-    contact: "تواصل معنا",
-    selectLanguage: "إختر لغة",
-    english: "الانجليزية",
-    arabic: "العربية",
-  },
-};
+import translations from "./translations.js";
 
 const languageSelector = document.querySelector("select");
 languageSelector.addEventListener("change", (event) => {
---
+++ translations.js
@@ -0,0 +1,20 @@
+const translations = {
+  en: {
+    home: "Home",
+    about: "About",
+    contact: "Contact us",
+    selectLanguage: "Select a language",
+    english: "English",
+    arabic: "Arabic",
+  },
+  ar: {
+    home: "الصفحة الرئيسية",
+    about: "من نحن",
+    contact: "تواصل معنا",
+    selectLanguage: "إختر لغة",
+    english: "الانجليزية",
+    arabic: "العربية",
+  },
+};
+
+export default translations;
